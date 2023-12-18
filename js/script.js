841795b4fd2d976044047c6a61f8939f40af72ee
unknown <yanuarmukti12@gmail.com>
2023-12-18 20:57:11 +0700

    Thirth commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function replaceName(){
+    let name = prompt("Please enter your name!","");
+    document.getElementById('nama').innerHTML = name;
+}
+
+document.getElementById('tombol').addEventListener("click",function(){
+    replaceName()
+});
+
+let nameInput = document.getElementById('name-input')
+
+document.getElementById('Kirim').addEventListener("click",function(){
+    prompt()
+})
