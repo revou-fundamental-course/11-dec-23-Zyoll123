95283b12bb1752c31720eb8d4f530c03249b5d09
unknown <yanuarmukti12@gmail.com>
2023-12-20 10:05:39 +0700

    Fourth commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
-function replaceName(){
-    let name = prompt("Please enter your name!","");
-    document.getElementById('nama').innerHTML = name;
+function setSenderUI(name, birthDate, gender, messages){
+    document.getElementById("sender-full-name").innerHTML = name;
+    document.getElementById("sender-birth-date").innerHTML = birthDate;
+    document.getElementById("sender-gender").innerHTML = gender;
+    document.getElementById("sender-messages").innerHTML = messages;
 }
 
-document.getElementById('tombol').addEventListener("click",function(){
-    replaceName()
-});
+function validateForm(){
+    const name = document.forms["message-form"]["full-name"].value;
+    const birthDate = document.forms["message-form"]["birth-date"].value;
+    const gender = document.forms["message-form"]["gender"].value;
+    const messages = document.forms["message-form"]["messages"].value;
 
-let nameInput = document.getElementById('name-input')
+    if(name == "" || birthDate == "" || gender == "" || messages == ""){
+        alert("Tidak boleh ada yang kosong");
+        return false;
+    }
 
-document.getElementById('Kirim').addEventListener("click",function(){
-    prompt()
-})
+    setSenderUI(name,birthDate,gender,messages);
+
+    return false;
+}
