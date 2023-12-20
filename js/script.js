6eefbbd07d37bc583843f86c132eba71ead1143c
unknown <yanuarmukti12@gmail.com>
2023-12-20 17:40:00 +0700

    fifth commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const slider = document.querySelector(".slider");
+    let isTransitioning = false;
+  
+    function nextSlide() {
+      if (!isTransitioning) {
+        isTransitioning = true;
+        setTimeout(() => {
+          const firstSlide = slider.firstElementChild;
+          slider.style.transition = "none";
+          slider.style.transform = "translateX(0)";
+          setTimeout(() => {
+            slider.style.transition = "transform 1s ease-in-out";
+            firstSlide.parentNode.appendChild(firstSlide);
+            isTransitioning = false;
+          }, 0);
+        }, 1000);
+      }
+    }
+  
+    setInterval(nextSlide, 3000); // Change slide every 3 seconds (adjust as needed)
+  });
+  
+
 function setSenderUI(name, birthDate, gender, messages){
     document.getElementById("sender-full-name").innerHTML = name;
     document.getElementById("sender-birth-date").innerHTML = birthDate;
